f58199eb26b3c50e73269e5d5b4e3fe5931724e4
ArtemKichigin <kevlar101@mail.ru>
2020-12-18 21:52:06 +0300

    second commmit

--- src/redux/types.js
+++ src/redux/types.js
@@ -1,3 +1,5 @@
 export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
 export const CHANGE_THEME = 'CHANGE_THEME'
+export const DISABLE_BUTTONS = 'DISABLE_BUTTON'
+export const ENABLE_BUTTONS = 'ENABLE_BUTTON'
